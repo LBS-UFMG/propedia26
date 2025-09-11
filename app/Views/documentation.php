64f8adc4b1691a2d7011231e41ad75b1a23c3386
Diego Mariano <diegomariano@ufmg.br>
2025-09-10 22:17:47 -0300

    Update documentation.php

--- app/Views/documentation.php
+++ app/Views/documentation.php
@@ -25,14 +25,14 @@
       <tr>
         <th scope="row">Unique entries</th>
         <td>51,082</td>
-        <td>20,632</td>
-        <td>71,714</td>
+        <td>0</td>
+        <td>51,082</td>
       </tr>
       <tr>
         <th scope="row">Duplicated entries</th>
         <td>27,066</td>
-        <td>0</td>
-        <td>27,066</td>
+        <td>20,632</td>
+        <td>47,698</td>
       </tr>
     </tbody>
     <tfoot class="table-light">
