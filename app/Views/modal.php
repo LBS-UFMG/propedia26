3c520204cbcd5af0c1705b05b06ab29dd454b048
Diego Mariano <diegomariano@ufmg.br>
2025-09-21 20:58:06 -0300

    cite

--- app/Views/modal.php
+++ app/Views/modal.php
@@ -59,7 +59,7 @@
           <div class="row">
             <div class="col-12 col-md-6">
 
-            <p class="small text-muted"><strong>BLAST algorithm</strong> searches for similar protein or peptide sequences by identifying short local matches (words) between the query and database sequences <a class="badge bg-dark" href="#" data-bs-placement="top" data-bs-toggle="tooltip" data-bs-title="Then extending these matches in both directions to form high-scoring segment pairs (HSPs), and finally scoring and ranking the alignments based on a substitution matrix and statistical significance. Parameters used for peptides search: -word_size 2 -task blastp-short -seg no -evalue 100000">?</a>.</p>
+              <p class="small text-muted"><strong>BLAST algorithm</strong> searches for similar protein or peptide sequences by identifying short local matches (words) between the query and database sequences <a class="badge bg-dark" href="#" data-bs-placement="top" data-bs-toggle="tooltip" data-bs-title="Then extending these matches in both directions to form high-scoring segment pairs (HSPs), and finally scoring and ranking the alignments based on a substitution matrix and statistical significance. Parameters used for peptides search: -word_size 2 -task blastp-short -seg no -evalue 100000">?</a>.</p>
               <h5><b>Input sequence</b></h5>
               <textarea id="txt_sequence" class="form-control" form="form_blast_run" name="sequence" rows="5" placeholder="Insert the sequence here (e.g.: TPYDINQML)"></textarea>
               <div hidden id="feedback_blast" class="alert alert-danger" role="alert">
@@ -79,7 +79,7 @@
             </div>
 
             <div class="col text-center mt-5">
-              <img class="w-75 img-thumbnail shadow p-3" src="<?=base_url('/img/blast.png')?>">
+              <img class="w-75 img-thumbnail shadow p-3" src="<?= base_url('/img/blast.png') ?>">
             </div>
           </div>
         </div>
@@ -93,20 +93,19 @@
 
 <!-- loading blast -->
 <div id="loading-blast">
-    <div class="text-center">
-        <img src="<?= base_url('/img/cocadito-loading.png') ?>" width="200px"><br>
-        <div class="spinner-border spinner-border-sm" role="status"></div>
-        <strong class="ms-2">Loading...</strong>
-    </div>
+  <div class="text-center">
+    <img src="<?= base_url('/img/cocadito-loading.png') ?>" width="200px"><br>
+    <div class="spinner-border spinner-border-sm" role="status"></div>
+    <strong class="ms-2">Loading...</strong>
+  </div>
 </div>
 
 <script>
   // Intercepta o submit do formulário
-document.getElementById("loading_blast").addEventListener("click", e => {
-  $('#loading-blast').css('visibility','visible').css('display','block');
-  document.getElementById("form_blast_run").submit();
-});
-
+  document.getElementById("loading_blast").addEventListener("click", e => {
+    $('#loading-blast').css('visibility', 'visible').css('display', 'block');
+    document.getElementById("form_blast_run").submit();
+  });
 </script>
 <!-- /.modal BLAST -->
 
@@ -122,106 +121,70 @@ document.getElementById("loading_blast").addEventListener("click", e => {
           <button type="button" class="btn" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         </div>
         <div class="modal-body">
-           <form id="form_probis_run" action="<?php echo base_url(); ?>search/binding" method="post" enctype="multipart/form-data">
+          <form id="form_probis_run" action="<?php echo base_url(); ?>search/binding" method="post" enctype="multipart/form-data">
 
-           <div class="row">
-            <div class="col">
-              <p class="small text-muted">
-                The search for similar binding sites in Propedia employs the <a class="link-dark" href="#" data-bs-placement="bottom" data-bs-toggle="tooltip" data-bs-title="ProBiS achieves this by aligning surface patches based on geometric and physicochemical properties, followed by statistical scoring of the alignments, thereby enabling the identification of proteins that share structurally conserved binding sites with the protein indicated here."><strong>ProBiS algorithm</strong></a>, which detects local structural similarities by comparing the three-dimensional surface of the queried protein binding site with those of proteins stored in the database. 
-              </p>
-              <p class="small text-muted"><strong>Enter the PDB code, target protein chain, and binding site residue numbers separated by commas (use hyphens to indicate ranges) <a class="badge bg-dark" href="#" data-bs-placement="top" data-bs-toggle="tooltip" data-bs-title="E.g.: 100,101,105-110 (i.e.: 100,101,105,106,107,108,109,110)">?</a>.</strong></p>
+            <div class="row">
+              <div class="col">
+                <p class="small text-muted">
+                  The search for similar binding sites in Propedia employs the <a class="link-dark" href="#" data-bs-placement="bottom" data-bs-toggle="tooltip" data-bs-title="ProBiS achieves this by aligning surface patches based on geometric and physicochemical properties, followed by statistical scoring of the alignments, thereby enabling the identification of proteins that share structurally conserved binding sites with the protein indicated here."><strong>ProBiS algorithm</strong></a>, which detects local structural similarities by comparing the three-dimensional surface of the queried protein binding site with those of proteins stored in the database.
+                </p>
+                <p class="small text-muted"><strong>Enter the PDB code, target protein chain, and binding site residue numbers separated by commas (use hyphens to indicate ranges) <a class="badge bg-dark" href="#" data-bs-placement="top" data-bs-toggle="tooltip" data-bs-title="E.g.: 100,101,105-110 (i.e.: 100,101,105,106,107,108,109,110)">?</a>.</strong></p>
 
-              <p>
-                <label class="badge bg-secondary">PDB ID</label>
-                <input name="pdb" type="text" class="form-control" placeholder="e.g.: 1a1m" required>
-              </p>
-              <p>
-                <label class="badge bg-secondary">Chain</label>
-                <input name="chain" type="text" class="form-control" placeholder="e.g.: A" required>
-              </p>
-              <p>
-                <label class="badge bg-secondary">Binding site residues</label>
-                <textarea name="residues" class="form-control" placeholder="e.g.: 60,62-82,146-171" rows="3" required></textarea>
-              </p>
+                <p>
+                  <label class="badge bg-secondary">PDB ID</label>
+                  <input name="pdb" type="text" class="form-control" placeholder="e.g.: 1a1m" required>
+                </p>
+                <p>
+                  <label class="badge bg-secondary">Chain</label>
+                  <input name="chain" type="text" class="form-control" placeholder="e.g.: A" required>
+                </p>
+                <p>
+                  <label class="badge bg-secondary">Binding site residues</label>
+                  <textarea name="residues" class="form-control" placeholder="e.g.: 60,62-82,146-171" rows="3" required></textarea>
+                </p>
 
-              <input name="search_binding_sites" type="submit" value="Search for proteins with similar binding sites" class="btn w-100 btn-primary mb-5 mt-3 btn-lg">
+                <input name="search_binding_sites" type="submit" value="Search for proteins with similar binding sites" class="btn w-100 btn-primary mb-5 mt-3 btn-lg">
 
+              </div>
+              <div class="col text-end">
+                <img src="<?= base_url('/img/bindingsite.png') ?>" class="w-75 ">
+              </div>
             </div>
-            <div class="col text-end">
-              <img src="<?=base_url('/img/bindingsite.png')?>" class="w-75 ">
-            </div>
-           </div>
-
-          <!-- <div id="3DmolViewer" style="min-height: 400px; margin:10px 0; width: 100%;"></div>
-          <div class="row">
-            <div class="col-md-5">
-              <div class="form-group row">
-                <label class="col-sm-5 col-form-label col-spacing_custom">
-                  <h5><b>Search from PDB:</b></h5>
+            <div id="feedback_upload" class="alert" role="alert" hidden></div>
+            <div id="fields" class="row" hidden>
+              <div class="col-md-12">
+                <h4><b>Protein chain select</b></h4>
+                <select id="selected_chain" name="selected_chain" class="form-control" style="width: 100%"></select>
+                <h4><b>Input residues id</b></h4>
+                <p style="color:gray;font-size:12px;">Separated by comma (',')</p>
+                <div class="row">
+                  <div class="col-md-8">
+                    <textarea id="residues_list" class="form-control" form="form_probis_run" name="residues_list" rows="3" placeho
+                      lder=""></textarea>
+                  </div>
+                  <div class="col-md-4">
+                    <label id="highlight_residues" class="btn btn-block btn-primary">Highlight residues surface
+                      <br>
+                      <i class="fa fa-eye"></i>
+                    </label>
+                  </div>
+                </div>
+                <h4><b>Search scope</b></h4>
+                <label class="radio-inline">
+                  <input type="radio" name="scope" value="ccd" checked>Only CCD <sup><a class="tip" href="#" data-placement="top" dat
+                      a-toggle="tooltip" title="Search in <?= number_format(1) //$ccd_number)
+                                                          ?> complex of Clustered Complex Dataset (faster).">?</a></sup>
                 </label>
-                <div class="col-sm-3 col-spacing_custom">
-                  <input id="search_pdb_id" class="form-control" placeholder="1a1m">
-                </div>
-                <div class="col-sm-4 col-spacing_custom">
-                  <label id="btn_pdb_id" class="btn btn-block btn-primary">
-                    <i class="fa fa-search"></i>
-                  </label>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-2">
-              <div class="text-center">
-                <h4><b>OR</b></h4>
-              </div>
-            </div>
-            <div class="col-md-5">
-              <label class="btn btn-block btn-info">
-                <i class="fa fa-upload"></i>&nbsp;Upload PDB
-                <input type="file" id="btn_pdb_file" accept=".pdb" style="display: none;">
-              </label>
-              <input id="input_pdb_file" name="input_pdb_file" hidden>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12">
-              <small><label id="loading_covid" class="btn-small btn-block btn-link">Loading sample: SARS-CoV-2 main protease (6lu7)</label></small>
-            </div>
-          </div> -->
-          <div id="feedback_upload" class="alert" role="alert" hidden></div>
-          <div id="fields" class="row" hidden>
-            <div class="col-md-12">
-              <h4><b>Protein chain select</b></h4>
-              <select id="selected_chain" name="selected_chain" class="form-control" style="width: 100%"></select>
-              <h4><b>Input residues id</b></h4>
-              <p style="color:gray;font-size:12px;">Separated by comma (',')</p>
-              <div class="row">
-                <div class="col-md-8">
-                  <textarea id="residues_list" class="form-control" form="form_probis_run" name="residues_list" rows="3" placeho
-                    lder=""></textarea>
-                </div>
-                <div class="col-md-4">
-                  <label id="highlight_residues" class="btn btn-block btn-primary">Highlight residues surface
-                    <br>
-                    <i class="fa fa-eye"></i>
-                  </label>
+                <label class="radio-inline">
+                  <input type="radio" name="scope" value="all">Whole database <sup><a class="tip" href="#" data-placement="top" data-
+                      toggle="tooltip" title="Search in <?= number_format(1) //$complex_number)
+                                                        ?> complex (slower).">?</a></sup>
+                </label>
+                <div hidden id="feedback_probis" class="alert alert-danger" role="alert">
+                  PDB file and residue list cannot by empty!
                 </div>
               </div>
-              <h4><b>Search scope</b></h4>
-              <label class="radio-inline">
-                <input type="radio" name="scope" value="ccd" checked>Only CCD <sup><a class="tip" href="#" data-placement="top" dat
-                    a-toggle="tooltip" title="Search in <?= number_format(1) //$ccd_number)
-                                                        ?> complex of Clustered Complex Dataset (faster).">?</a></sup>
-              </label>
-              <label class="radio-inline">
-                <input type="radio" name="scope" value="all">Whole database <sup><a class="tip" href="#" data-placement="top" data-
-                    toggle="tooltip" title="Search in <?= number_format(1) //$complex_number)
-                                                      ?> complex (slower).">?</a></sup>
-              </label>
-              <div hidden id="feedback_probis" class="alert alert-danger" role="alert">
-                PDB file and residue list cannot by empty!
-              </div>
             </div>
-          </div>
         </div>
         <div class="modal-footer">
           <input id="run_probis_btn" type="submit" class="btn btn-success" value="Run ProBiS NOW" style="display: none;">
@@ -232,3 +195,40 @@ document.getElementById("loading_blast").addEventListener("click", e => {
   </div>
 </div>
 
+<!-- MODAL: CITE -->
+<div class="modal fade" tabindex="-1" id="cite" role="dialog">
+  <div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header bg-dark">
+        <div class="text-center">
+          <img width="150" class="me-3" src="<?php echo base_url('/img/logo_propedia.svg'); ?>">
+        </div>
+        <button type="button" class="btn" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      </div>
+      <div class="modal-body">
+        <h4 class="" style="color:#031430"><strong>How to cite:</strong></h4>
+        <label class="badge bg-dark">Original paper (2021)</label>
+        <p class="small" id="browse1"> Martins, P.M., Santos, L.H., Mariano, D. et al. <strong>Propedia: a database for protein–peptide identification based on a hybrid clustering algorithm.</strong> BMC Bioinformatics 22, 1 (2021). doi: <a href="https://doi.org/10.1186/s12859-020-03881-z" target="_blank">10.1186/s12859-020-03881-z</a>
+        </p>
+
+        <label class="badge bg-dark">Propedia v2.3 (2023)</label>
+        <p class="small" id="browse2"> Martins P, Mariano D, Carvalho FC, Bastos LL, Moraes L, Paixão V and Cardoso de Melo-Minardi R (2023). <strong>Propedia v2.3: A novel representation approach for the peptide-protein interaction database using graph-based structural signatures</strong>. Front. Bioinform. 3:1103103. doi: <a href="https://doi.org/10.3389/fbinf.2023.1103103" target="_blank">10.3389/fbinf.2023.1103103</a>
+        </p>
+
+        <label class="badge bg-dark">Propedia 26 (2026)</label>
+        <p class="small" id="browse3"><em>In development.</em>
+        </p>
+      </div>
+
+      <div class="modal-footer">
+        <img height="50" class="me-3" src="<?php echo base_url('/img/dcc_b.svg'); ?>">
+        <img height="50" class="me-3" src="<?php echo base_url('/img/ufmg_b.svg'); ?>">
+
+        <button type="button" class="btn btn-dark py-4 px-5" data-bs-dismiss="modal">Fechar</button>
+      </div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.modal SOBRE -->
