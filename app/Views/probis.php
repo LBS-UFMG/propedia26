34ddb8c76808f8160729e94d5c24b4fbd266cf03
Diego Mariano <diegomariano@ufmg.br>
2025-09-22 10:46:33 -0300

    Update probis.php

--- app/Views/probis.php
+++ app/Views/probis.php
@@ -168,7 +168,7 @@ selected">
                   const color = colors[i % colors.length];
 
                   if(color == 'orangered'){
-                     glviewer.setStyle({ chain: chain }, { stick: { colorscheme: 'orangeCartoon' } });
+                     glviewer.setStyle({ chain: chain }, { stick: { color: 'orangered' }, cartoon: { color: color } });
                   }
                   else{
                      glviewer.setStyle({ chain: chain }, { cartoon: { color: color } });
