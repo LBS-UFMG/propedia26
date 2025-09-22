09b87ab2a120b3ef2af590d141d17942abc80ddd
Diego Mariano <diegomariano@ufmg.br>
2025-09-22 15:41:47 -0300

    REDUNDANT SEQUENCES

--- app/Views/clusters.php
+++ app/Views/clusters.php
@@ -2,16 +2,79 @@
 <?= $this->section('conteudo') ?>
 <!-- Conteúdo personalizado -->
 
-<h1>
-    Clusters
-</h1>
+<div class="container-fluid">
+    <h1><strong>Clusters</strong></h1>
+    <p class="text-muted small">
+        Propedia is a database geared toward machine learning applications. Therefore, it presents data grouped using different clustering methods.
+    </p>
 
-<h2>Redundant sequences</h2>
-<h2>Classifications (PDB)</h2>
-<h2>Sequence</h2>
-<h2>Interface</h2>
-<h2>Binding site</h2>
+    <nav>
+        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-1" type="button" role="tab" aria-selected="true">Redundant sequences</button>
+
+            <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-2" type="button" role="tab" aria-selected="false">Classifications (PDB)</button>
+
+            <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-3" type="button" role="tab" aria-selected="false">Sequence clusters</button>
+
+            <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-4" type="button" role="tab" aria-selected="false">Interface clusters</button>
+
+            <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-5" type="button" role="tab" aria-selected="false">Binding site clusters</button>
+
+            <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-6" type="button" role="tab" aria-selected="false">CSM-peptides</button>
+        </div>
+    </nav>
+
+    <div class="tab-content" id="nav-tabContent">
+        <div class="tab-pane fade show active p-4" id="nav-1" role="tabpanel" tabindex="0">
+            <table class="table table-hover table-striped table-condensed" id="redundant">
+                <thead>
+                    <th>Leader</th>
+                    <th><em>n</em>
+                    <th>Redundant sequences (+leader)</th>
+                </thead>
+                <tbody></tbody>
+            </table>
+
+            <a href="<?=base_url('/data/redundant.tsv')?>" class="btn btn-primary btn-lg mt-5 w-100">Download redundant sequences (tsv file: ~1MB)</a>
+        </div>
+        <div class="tab-pane fade" id="nav-2" role="tabpanel" tabindex="0">...</div>
+        <div class="tab-pane fade" id="nav-3" role="tabpanel" tabindex="0">...</div>
+        <div class="tab-pane fade" id="nav-4" role="tabpanel" tabindex="0">...</div>
+        <div class="tab-pane fade" id="nav-5" role="tabpanel" tabindex="0">...</div>
+        <div class="tab-pane fade" id="nav-6" role="tabpanel" tabindex="0">...</div>
+    </div>
+
+</div><!-- / container fluid -->
 
 
+<script>
+    const tabela_redundante = fetch('<?=base_url('/data/redundant.tsv')?>');
+
+    tabela_redundante.then(d => d.text()).then((dados)=>{
+        const linhas = dados.trim().split('\n').map(linha => linha.split('\t'));
+        const table = $("#redundant").DataTable({
+            data: linhas,
+            paging: true,
+            pageLength: 10,
+            deferRender: true,
+            processing: true,
+            columnDefs: [
+                {
+                targets: 0, // primeira coluna
+                render: function (data, type, row) {
+                    return `<a href="<?=base_url('/entry')?>/${data}" target="_blank">${data}</a>`;
+                }
+                }
+            ]
+            // outras opções que achar necessárias...
+            // dom: 'Bfrtip',
+            // "buttons": [
+            //     'csv', 'excel',
+            // ],
+        });
+    });
+
+    
+</script>
 <!-- / FIM Conteúdo personalizado -->
 <?= $this->endSection() ?>
