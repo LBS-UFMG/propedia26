cff627f4409651622e26c68821debb309945d492
Diego Mariano <diegomariano@ufmg.br>
2025-12-05 09:56:39 -0300

    Update explore.php

--- app/Views/explore.php
+++ app/Views/explore.php
@@ -395,7 +395,7 @@
                     <tbody>
                     </tbody>
                 </table>
-                <p id="loading-data" class="text-center text-muted small">Wait... loading data...<br><div class="spinner-border spinner-border-sm" role="status"></div></p>
+                <p id="loading-data" class="text-center text-muted small"><div class="spinner-border spinner-border-sm" role="status"></div>Wait... loading data...</p>
             </div>
         </div>
     </div>
