a175473c7595546063eec0ddadb66caa2cfa3639
Diego Mariano <diegomariano@ufmg.br>
2025-12-10 10:46:20 -0300

    Update entry.php

--- app/Views/entry.php
+++ app/Views/entry.php
@@ -351,7 +351,7 @@
 
             <p class="bg-light p-3 rounded small">
                 <label class="badge bg-dark">Interface: <?= substr_count($info[14],",")+1 ?> residues</label>
-                <label class="badge bg-secondary">Chain: <?= $info[27] ?></label><br>
+                <label class="badge bg-secondary">Chain: <?= $info[27] ?></label>
                 <span class="pt-2"><?= str_replace(',', ', ', $info[14]) ?></span>
             </p>
 
